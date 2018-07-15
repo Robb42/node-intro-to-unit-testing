eed187578dc6928009e27cfa759cb7292ee553f1
Rob Bertini <rob.bertini@gmail.com>
2018-07-15 18:07:11 -0400

    testing with mocha and chai solution

---
+++ test/test-fizz-buzzer.js
@@ -0,0 +1,35 @@
+const expect = require('chai').expect;
+const fizzBuzzer = require('../fizzBuzzer.js');
+
+describe('fizzBuzzer', function () {
+    it('should return fizz-buzz if num is divisible by 3 and 5', function () {
+        const normalCases = [
+            {num: 15, expected: 'fizz-buzz'},
+            {num: 30, expected: 'fizz-buzz'},
+            {num: 45, expected: 'fizz-buzz'},
+            {num: 5, expected: 'buzz'},
+            {num: 10, expected: 'buzz'},
+            {num: 20, expected: 'buzz'},
+            {num: 3, expected: 'fizz'},
+            {num: 6, expected: 'fizz'},
+            {num: 9, expected: 'fizz'}
+        ];
+        normalCases.forEach(function(input) {
+            const answer = fizzBuzzer(input.num);
+            expect(answer).to.equal(input.expected);
+        });
+    });
+
+    it('should raise an error if num is non-numeric', function () {
+        const badInputs = [
+            ['a'],
+            [false],
+            ['1']
+        ];
+        badInputs.forEach(function(input) {
+            expect(function() {
+                fizzBuzzer(input[0]);
+            }).to.throw(Error);
+        });
+    });
+});
